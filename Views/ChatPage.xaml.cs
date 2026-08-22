374474d2dd203b8b33f91677155ec07baa382cb2
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2026-08-22 00:19:10 -0300

    feat: enviar mensagem com enter

--- Views/ChatPage.xaml.cs
+++ Views/ChatPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using ElosWin.ViewModels;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Windows.System;
 
 namespace ElosWin.Views;
 
@@ -10,5 +12,16 @@ public sealed partial class ChatPage : Page
     public ChatPage()
     {
         InitializeComponent();
+    }
+
+    private void ChatInput_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == VirtualKey.Enter)
+        {
+            if (ViewModel.SendMessageCommand.CanExecute(null))
+                ViewModel.SendMessageCommand.Execute(null);
+
+            e.Handled = true;
+        }
     }
 }
